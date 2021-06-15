27f9163467820d64da4f0e6a913feae7f37e5181
Gabriel Griffin <griffinsilva10@gmail.com>
2021-06-15 15:13:45 -0600

    Rediseno_vist_login
    Rediseño de nuestra vista de Login

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,17 @@
+<template>
+    <inertia-link :href="'/'">
+        <img class="w-80" :src="image" alt="Logo grifftagram">
+    </inertia-link>
+</template>
+
+<script>
+import image from '../../image/grifftagram.png'
+
+export default {
+    data() {
+        return {
+            image:image
+        }
+    },
+}
+</script>
